8740770cd749a9e5206f31532c4a8703e2cbe690
uniuniworld <uniuniunu2525@gmail.com>
2023-04-17 23:45:26 +0900

    first commit

---
+++ SampleMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleMVVM
+//
+//  Created by Takahiro Kirifu on 2023/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
